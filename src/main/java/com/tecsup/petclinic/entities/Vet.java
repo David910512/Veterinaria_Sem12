0e90bdba9987814d8195ae98d869fb033d8985f3
Betuel <betuel.arones@tecsup.edu.pe>
2025-11-03 16:36:24 -0500

    VetDTO, Vet Mapeado

--- src/main/java/com/tecsup/petclinic/entities/Vet.java
+++ src/main/java/com/tecsup/petclinic/entities/Vet.java
@@ -11,6 +11,7 @@ import java.util.Set;
  * @author jgomezm
  *
  */
+
 @NoArgsConstructor
 @Entity(name = "vets")
 @Data
---
+++ src/main/java/com/tecsup/petclinic/mapper/VetMapper.java
@@ -0,0 +1,31 @@
+package com.tecsup.petclinic.mapper;
+
+import com.tecsup.petclinic.dtos.VetDTO;
+import com.tecsup.petclinic.entities.Vet;
+
+public class VetMapper {
+
+
+    /**
+     * Convert DTO to Entity
+     * @param dto
+     * @return
+     */
+    public Vet mapToEntity(VetDTO dto) {
+        if (dto == null) return null;
+        return new Vet(
+            //    dto.getId(),
+            //    dto.getFirstName(),
+            //    dto.getLastName()
+        );
+    }
+
+    public VetDTO mapToDto(Vet entity) {
+        if (entity == null) return null;
+        return new VetDTO(
+                entity.getId(),
+                entity.getFirstName(),
+                entity.getLastName()
+        );
+    }
+}
